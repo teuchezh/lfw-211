59c54520e272a9ae2dda340e930a67663591b3f4
teuchezh <teuchezh666@yandex.ru>
2023-04-19 11:59:42 +0300

    lab 15

--- ch-15/labs-2/index.js
+++ ch-15/labs-2/index.js
@@ -2,8 +2,16 @@
 
 const { spawn } = require('child_process')
 
-function exercise (command, args) {
-  return spawn(command, args)
+function exercise(command, args) {
+
+  return spawn(command, args, {
+    stdio: ['ignore', 'inherit', 'pipe'],
+  })
 }
 
 module.exports = exercise
+
+// Опция options.stdio используется для настройки каналов, которые устанавливаются между родительским и дочерним процессом
+// pipe - канал между родительским и дочерним процессом
+// inherit - канал между родительским и дочерним процессом, но вместо создания нового канала, используется канал родительского процесса
+// ignore - канал не создается
